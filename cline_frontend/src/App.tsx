fe80a175e12875487dd910e6c3cc59d450b1cb84
VijayBharath22 <vb222006@gmail.com>
2026-09-10 14:27:39 +0530

    clean

--- cline_frontend/src/App.tsx
+++
@@ -1,5 +0,0 @@
-import { CommandCenter } from './components/layout/CommandCenter';
-
-export default function App() {
-  return <CommandCenter />;
-}
--- cline_frontend/src/utils/format.ts
+++
@@ -1,8 +0,0 @@
-export function formatTimestamp(iso: string): string {
-  return new Date(iso).toLocaleTimeString('en-IN', {
-    hour: '2-digit',
-    minute: '2-digit',
-    second: '2-digit',
-    hour12: false,
-  });
-}
